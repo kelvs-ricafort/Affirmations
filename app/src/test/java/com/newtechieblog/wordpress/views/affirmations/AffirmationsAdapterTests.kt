8d23b33433ab117baa31c98046d0dd5b01736f50
Kelvin Ricafort <kelvs.r@outlook.com>
2023-11-28 14:11:39 +0800

    Added Tests

---
+++ app/src/test/java/com/newtechieblog/wordpress/views/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,24 @@
+package com.newtechieblog.wordpress.views.affirmations
+
+import android.content.Context
+import com.newtechieblog.wordpress.views.affirmations.adapter.ItemAdapter
+import com.newtechieblog.wordpress.views.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
